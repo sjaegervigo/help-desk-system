7a1904c52271665acc3de5266391e4bc8a2e1623
sjaegervigo <sjaegervigo@gmail.com>
2026-03-25 23:56:00 -0500

    Ticket class implemented

--- demo/src/main/java/com/helpdesk/demo/model/Ticket.java
+++ demo/src/main/java/com/helpdesk/demo/model/Ticket.java
@@ -1,4 +1,31 @@
 package com.helpdesk.demo.model;
 
+import jakarta.persitance.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tickets")
+@Data
 public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    private String estado;
+    private String prioridad;
+
+    private LocalDateTime fechaCreacion;
+
+    @PrePersist
+    protected void onCreate(){
+        fechaCreation = LocalDateTime.now();
+        if ( estado ==null) estado = "OPEN"
+    }
 }
